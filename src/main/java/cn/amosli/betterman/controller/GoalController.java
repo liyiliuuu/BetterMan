d29bc5d4523e30e53f0298cfb770385b6cd27d4a
liyiliuuu <970912723@qq.com>
2018-05-18 15:27:55 +0800

    init commit

---
+++ src/main/java/cn/amosli/betterman/controller/GoalController.java
@@ -0,0 +1,34 @@
+package cn.amosli.betterman.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @deprecated this controller is to operate the goal init table
+ * @author Amos
+ * @version v1
+ */
+
+@RestController
+public class GoalController {
+    /**
+     * 新增目标
+     */
+
+    /**
+     * 修改目标
+     */
+
+    /**
+     * 删除目标
+     */
+
+    /**
+     * 查询单个目标信息
+     */
+
+    /**
+     * 查询指定用户所有目标列表
+     */
+
+
+}
